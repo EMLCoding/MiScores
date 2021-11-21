5ecc3a2f4290344b107fcf42e2b815586f0e80f0
Edu-Estudios <eduardo.ml.trabajo@gmail.com>
2021-11-21 11:38:46 +0100

    Initial Commit

---
+++ MiScoresApp/MiScoresAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiScoresAppApp.swift
+//  MiScoresApp
+//
+//  Created by Eduardo Martin Lorenzo on 21/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MiScoresAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
